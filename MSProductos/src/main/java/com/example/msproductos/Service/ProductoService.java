cd3ad6ef96f2446cab95164f0019a3ce98cb83e7
pinguica13-hue <pinguica13@gmail.com>
2025-09-01 18:32:12 -0500

    Create ProductoService.java

---
+++ MSProductos/src/main/java/com/example/msproductos/Service/ProductoService.java
@@ -0,0 +1,33 @@
+package com.example.msproductos.service;
+
+import com.example.msproductos.model.Producto;
+import com.example.msproductos.repository.ProductoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductoService 
+{
+  private final ProductoRepository repo;
+  public ProductoService(ProductoRepository repo) 
+     {
+  this.repo = repo;
+  }
+  
+  public Producto crear(Producto p) 
+  {
+  return repo.save(p);
+  }
+  
+  public Optional<Producto> obtener(Long id) 
+  {
+  return repo.findById(id);
+  }
+  
+  public List<Producto> listar() 
+  {
+  return repo.findAll();
+  }
+}
